befeeed8d9ddef62d1945249fb6b08da2bb59c30
ChoeHwanseong <newo8995@gmail.com>
2024-08-01 17:53:45 +0900

    푸터 헤더 최종

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import '../styles/Footer.css';
+import styles from '../styles/Footer.module.css'; // CSS 모듈 파일을 불러옵니다.
 
 const Footer = ({ className, ...props }) => {
   return (
-    <div className={"footer " + className}>
-      <div className="footer-left">
-        <div className="trip-maven">TripMaven</div>
-        <div className="happy-find-reporter">
+    <div className={`${styles.footer} ${className}`}>
+      <div className={styles.footerLeft}>
+        <div className={styles.tripMaven}>TripMaven</div>
+        <div className={styles.happyFindReporter}>
           <span>
-            <span className="happy-find-reporter-span">
+            <span className={styles.happyFindReporterSpan}>
               여행 전문가들의 사이트
               <br />
             </span>
-            <span className="happy-find-reporter-span2">Happy find reporter</span>
+            <span className={styles.happyFindReporterSpan2}>Happy find reporter</span>
           </span>
         </div>
       </div>
-      <div className="footer-right">
-        <div className="footer-links">
-          <a href="#" className="footer-link">사이트소개</a>
-          <a href="#" className="footer-link">이용약관</a>
-          <a href="#" className="footer-link">1:1문의</a>
-          <a href="#" className="footer-link">고객센터</a>
+      <div className={styles.footerRight}>
+        <div className={styles.footerLinks}>
+          <a href="#" className={styles.footerLink}>사이트소개</a>
+          <a href="#" className={styles.footerLink}>이용약관</a>
+          <a href="#" className={styles.footerLink}>1:1문의</a>
+          <a href="#" className={styles.footerLink}>고객센터</a>
         </div>
-        <div className="footer-icons">
-          <img src="/images/naver.png" alt="Naver" className="sns-icon" />
-          <img src="/images/removebg-preview-10.png" alt="Kakao" className="sns-icon" />
-          <img src="/images/google0.svg" alt="Google" className="sns-icon" />
+        <div className={styles.footerIcons}>
+          <img src="/images/naver.png" alt="Naver" className={styles.snsIcon} />
+          <img src="/images/kakao.png" alt="Kakao" className={styles.snsIcon} />
+          <img src="/images/google.png" alt="Google" className={styles.snsIcon} />
         </div>
       </div>
     </div>
