36c1f72bb4f351dba11158fc8dde3ec0a652b682
ChoeHwanseong <newo8995@gmail.com>
2024-08-01 18:21:11 +0900

    찌찐 최종(헤더푸터) 하지만 위에 메뉴 바뀌어야함

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom
 import React from 'react';
 import Home from "./pages/home";
 import Template from "./pages/template";
-
+import './styles/App.css';
 
 
 function App() {
