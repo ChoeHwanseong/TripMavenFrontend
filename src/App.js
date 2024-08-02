6aae9beb089f8d3cca72d1ba03e388815f437711
LeeGyuRim <ejrqorl@naver.com>
2024-08-02 09:45:04 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Home from "./pages/home";
 import Template from "./pages/template";
 import Login from "./pages/login/login";
+import CSBoard from './pages/csBoard';
 
 function App() {
   return <>
@@ -12,6 +13,7 @@ function App() {
       <Route element={<Template />}>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/cs" element={<CSBoard/>}></Route>
       </Route>
 
     </Routes>
