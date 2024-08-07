bb9d2c2bd344f0dd27a13936f83fdb029985a4e0
ChoWooJINnn <6795hyojung@gmail.com>
2024-08-07 18:09:11 +0900

    상품확인, 결제, 결제 확인

---
+++ src/pages/payment/paymentConfirm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from '../../styles/payment/PaymentConfirm.module.css';
+
+const PaymentConfirmation = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <h1 className={styles.title}>결제 확인 창</h1>
+        
+        <div className={styles.inputGroup}>
+          <label htmlFor="quantity" className={styles.label}>수량</label>
+          <input type="text" id="quantity" value="1" readOnly className={styles.input} />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <label htmlFor="paymentMethod" className={styles.label}>결제 방법</label>
+          <input type="text" id="paymentMethod" value="TossPay" readOnly className={styles.input} />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <label htmlFor="paymentTime" className={styles.label}>결제 시간</label>
+          <input type="text" id="paymentTime" value="2024-08-05 19:17:15" readOnly className={styles.input} />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <label htmlFor="paymentStatus" className={styles.label}>결제 상태</label>
+          <input type="text" id="paymentStatus" value="Pending" readOnly className={styles.input} />
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <button className={styles.confirmButton}>결제 완료</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentConfirmation;
