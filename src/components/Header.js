7a84d5cf47be1e148c8c2fb59e0c28553ddb5d5f
ChoiGaHeun <aprilcgh@naver.com>
2024-08-15 21:31:45 +0900

    d

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,6 @@ import React, { useContext, useEffect, useState } from 'react';
 import styles from '../styles/components/Header.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
-
 import { useLocation, useNavigate } from 'react-router-dom';
 import { productFetchTitleAndContent } from '../utils/productData';
 import { menuData } from '../config/MyPageEndPoint';
@@ -30,9 +28,12 @@ const Header = () => {
     const location = useLocation();
     
     const navigate = useNavigate();
-    // 입력한 검색어 관리
+    
+    //로그인한 사용자 role 가져오기(로그인 구현하면 변경할 예정)
+    // + 입력한 검색어 관리(전역 스테이트)
     const {role, setRole,searchKeyword, setSearchKeyword} = useContext(RoleContext);
-    //로그인한 사용자 role 가져오기
+    
+    //role에 따라서 마이페이지에 있는 메뉴 변경하기
     let menuList = menuData[role]
 
     // Modal 상태 관리
@@ -40,45 +41,28 @@ const Header = () => {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-
-    
-   
-
     // 검색어에 따라 searchPost 상태 업데이트
     const handleInputPost = (event) => {
         setSearchKeyword(event.target.value);
     };
 
-    // enter를 이용한 검색
+    // 엔터키로 이동
     const handleEnterPress = (event) => {
         //console.log(event.key); //디버그용
-        if (event.key === 'Enter') {
-        performSearch();
-        
-        }
+        if (event.key === 'Enter') handleNavigatePage();
     }
-    // 검색
-    const performSearch = async () => {
+    const handleNavigatePage = ()=>{
         console.log('검색 실행:', searchKeyword);
-        try {
-            //const results = await productFetchTitleAndContent(searchPost);
-            //console.log('검색 결과:', results);
-            navigate('/product'); 
-        } catch (error) {
-            console.error('검색 중 에러 발생:', error);
-        }
-    };
+        navigate('/product');
+    }
     
-    //role에 따라서 마이페이지에 있는 메뉴 변경하기
-    
-   
     //console.log(role); //디버그용
     //console.log(menuList); //디버그용
 
     return (
         <header className={styles.header}>
             <div className={styles.headerFrame}>
-                <button className={styles.logoButton} onClick={() => {setSearchKeyword(''); navigate('/home');}}>TripMaven</button>
+                <button className={styles.logoButton} onClick={() => {navigate('/home');}}>TripMaven</button>
                 <button className={styles.navbutton2} onClick={()=>{setRole('user')}}>고객</button>
                 <button className={styles.navbutton2} onClick={()=>{setRole('guide')}}>가이드</button>
                 <button className={styles.navbutton2} onClick={()=>{setRole('admin')}}>관리자</button>
@@ -88,11 +72,11 @@ const Header = () => {
                             type="text"
                             className={styles.searchInput}
                             placeholder="검색어를 입력하세요"
-                            value={location.pathname.contains('')?searchKeyword:''}
+                            value={searchKeyword}
                             onChange={handleInputPost}
                             onKeyDown={handleEnterPress}
                         />
-                        <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} onClick={()=>performSearch()}/>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} onClick={handleNavigatePage}/>
                     </div>
                     <div className={styles.navFrame}>
                         <div className={styles.navItems}>
