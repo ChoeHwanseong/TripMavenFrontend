4318eda6e70d7d4f6893bc9fd94a99d9eb1b0f13
ChoeHwanseong <newo8995@gmail.com>
2024-08-16 10:54:45 +0900

    다시!

--- src/utils/memberData.js
+++ src/utils/memberData.js
@@ -54,7 +54,7 @@ export const FormLogin = async (form) =>{
     let role = response.data.isAdmin ? "admin" : "user";
     role = response.data.isGuide ? "guide" : "user";
     window.localStorage.setItem("role", role);
-    window.localStorage.setItem("membersId", membersId);
+    window.localStorage.setItem("membersId", response.data.membersId);
     window.localStorage.setItem("refresh", response.data.refresh);
   }
 
