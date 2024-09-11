a87c2b7be4f9992b38f0d4c3a538693cdbd4a083
JangLeeJun <dlwns0784@gmail.com>
2024-09-11 22:26:39 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ function App() {
           <Route path='/bigChat' element={<BigChat/>}/>
           {/*<Route path="/guideaskdetailsview/:id" element={<GuideAskDetailsView />} />*/}
 
-          <Route path='/bigChat/:id/:roomId' element={<BigChat/>}/>
+          <Route path='/bigChat/:id' element={<BigChat/>}/>
           <Route path='/chattingRoom' element={<ChattingRoom/>}/>
           
           <Route path='/askdetailsview/:id' element={<AskDetailsView/>}/>
