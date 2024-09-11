3b796227e2c01984e1fa11efeeccd6f547a88ed8
ChoeHwanseong <newo8995@gmail.com>
2024-09-11 23:33:48 +0900

    전체 목록에서 가이드거나 관리자면 상품등록 만들어 두었습니다.

--- src/pages/productPage/ProductBoard.js
+++ src/pages/productPage/ProductBoard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useInView } from 'react-intersection-observer';
 import styles from '../../styles/productPage/ProductBoard.module.css';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -7,8 +7,9 @@ import Box from '@mui/material/Box';
 import { postsAllGet, postsCityGet, postsKeywordGet } from '../../utils/postData';
 import YouTubeSearch from '../guidemypage/YouTubeSearch';
 import { fetchFiles } from '../../utils/fileData';
-import { Rating } from '@mui/material';
-import  defaultimg  from '../../images/default_profile.png';
+import { Button, Rating } from '@mui/material';
+import defaultimg from '../../images/default_profile.png';
+import { TemplateContext } from '../../context/TemplateContext';
 
 const ProductBoard = () => {
     const location = useLocation();
@@ -20,7 +21,8 @@ const ProductBoard = () => {
     const [page, setPage] = useState(0); // 페이지(20개씩임)
     const [hasMore, setHasMore] = useState(true); // 더 불러올 데이터가 있는지 여부
     const [loading, setLoading] = useState(false);
-    
+    const { memberInfo } = useContext(TemplateContext);
+
     const { ref, inView } = useInView({
         threshold: 0, // 요소가 100% 보일 때 트리거
     });
@@ -47,9 +49,9 @@ const ProductBoard = () => {
             })
         );
 
-        console.log('productsWithFiles: ',productsWithFiles);
+        console.log('productsWithFiles: ', productsWithFiles);
         setProducts(productsWithFiles);
-        console.log('products: ',products);
+        console.log('products: ', products);
 
         setPage((prevPage) => prevPage + 1); // 다음 페이지로 설정
         if (results.length < 20) {
@@ -81,9 +83,18 @@ const ProductBoard = () => {
 
     return (
         <div className={styles.container}>
-            <div>
+            <div className='d-flex justify-content-between'>
                 <h1 style={{ marginLeft: '40px' }}>{city} 상품 목록</h1>
                 {!city && (keyword !== '' && <h3 style={{ marginLeft: '40px' }}>'{keyword}'에 대한 검색 결과입니다</h3>)}
+                {(memberInfo.role == 'GUIDE' || memberInfo.role == 'ADMIN') &&
+                    <Button
+                        variant="contained"
+                        sx={{ backgroundColor: '#0066ff', '&:hover': { backgroundColor: '#0056b3' } }}
+                        onClick={() => navigate(`/guidePost/${memberInfo.id}`)}
+                    >
+                        게시물 등록 하기
+                    </Button>
+                }
             </div>
             <div className={styles.productList}>
                 {products.map((product, index) => (
@@ -110,48 +121,48 @@ const ProductBoard = () => {
                         <div className={styles.productInfo}>
                             <h3 style={{ fontSize: '2.5rem' }}>[{product.city}][{product.day}] {product.title}</h3>
                             <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '30px' }}>
-                            {/* 프로필 이미지 (디폴트 이미지 설정) */}
-                            <img 
-                                src={defaultimg} // 디폴트 프로필 이미지
-                                alt="profile" 
-                                style={{
-                                    width: '40px', 
-                                    height: '40px', 
-                                    borderRadius: '50%', 
-                                    objectFit: 'cover',
-                                    marginRight: '10px'
-                                }}
-                            />
-                            {/* 이름 */}
-                            <p style={{ margin: 0 }}>{product.member.name}</p>
-                        </Box>
-                           
+                                {/* 프로필 이미지 (디폴트 이미지 설정) */}
+                                <img
+                                    src={defaultimg} // 디폴트 프로필 이미지
+                                    alt="profile"
+                                    style={{
+                                        width: '40px',
+                                        height: '40px',
+                                        borderRadius: '50%',
+                                        objectFit: 'cover',
+                                        marginRight: '10px'
+                                    }}
+                                />
+                                {/* 이름 */}
+                                <p style={{ margin: 0 }}>{product.member.name}</p>
+                            </Box>
+
                             <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
-                            <Box sx={{ marginRight: '10px' }}>
-                                {product.review || '0'} 건의 리뷰
+                                <Box sx={{ marginRight: '10px' }}>
+                                    {product.review || '0'} 건의 리뷰
+                                </Box>
+                                <Rating
+                                    name="half-rating-read"
+                                    defaultValue={3.5}
+                                    precision={0.5}
+                                    readOnly
+                                    sx={{ marginRight: '10px' }}
+                                />
+                                <Box sx={{ marginRight: '10px', ml: 3 }}>
+                                    AI 점수
+                                </Box>
+                                <Rating
+                                    name="half-rating-read"
+                                    defaultValue={4.5}
+                                    precision={0.5}
+                                    readOnly
+                                    sx={{
+                                        '& .MuiRating-iconFilled': {
+                                            color: 'blue',
+                                        },
+                                    }}
+                                />
                             </Box>
-                            <Rating 
-                                name="half-rating-read" 
-                                defaultValue={3.5} 
-                                precision={0.5} 
-                                readOnly 
-                                sx={{ marginRight: '10px' }} 
-                            />
-                            <Box sx={{ marginRight: '10px' ,ml : 3}}>
-                                AI 점수
-                            </Box>
-                            <Rating 
-                                name="half-rating-read" 
-                                defaultValue={4.5} 
-                                precision={0.5} 
-                                readOnly 
-                                sx={{
-                                    '& .MuiRating-iconFilled': {
-                                        color: 'blue',
-                                    },
-                                }} 
-                            />
-                        </Box>
 
                             <div className={styles.tags}>
                                 {product.hashtag && product.hashtag.split('#').filter(Boolean).map((tag, index) => (
@@ -174,7 +185,7 @@ const ProductBoard = () => {
 
             <div className="App">
                 <h1 className={styles.youtubeHeading}>YouTube Video Search</h1>
-                <YouTubeSearch keyword={keyword} city={city}/>
+                <YouTubeSearch keyword={keyword} city={city} />
             </div>
         </div>
     );
