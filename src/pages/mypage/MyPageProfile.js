b004633cbb2bc844074ddf9f1166cdb197239db7
ChoeHwanseong <newo8995@gmail.com>
2024-09-21 19:02:49 +0900

    관리자, 회원목록 반영

--- src/pages/mypage/MyPageProfile.js
+++ src/pages/mypage/MyPageProfile.js
@@ -1,20 +1,33 @@
-import React, { useContext, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Box, Button, TextField, Typography, Avatar, Grid } from '@mui/material';
 import styles from '../../styles/mypage/MyProfile.module.css';
 import defaultImage from '../../images/default_profile.png';
 import { TemplateContext } from '../../context/TemplateContext';
-import { deleteProfile, logout } from '../../utils/memberData';
+import { deleteProfile, fetchedData } from '../../utils/memberData';
 
 const MypageProfile = () => {
   const template = useContext(TemplateContext);
   const navigate = useNavigate();
+  const { id } = useParams();
+  const [userInfo, setUserInfo] = useState(template.memberInfo);
+  console.log(userInfo)
+
+  useEffect(() => {
+    if (template.memberInfo && template.memberInfo.id !== id) {
+      const getMember = async (id) => {
+        const memberData = await fetchedData(id);
+        setUserInfo(memberData);
+      }
+      getMember(id);
+    };
+  },[id, template.memberInfo])
 
   const deleteMember = async () => {
     const confirmed = window.confirm("진짜 탈퇴하시겠습니까?");
     if (confirmed) {
       // 회원 탈퇴 로직
-      deleteProfile(template.memberInfo.id);
+      deleteProfile(userInfo.id);
       alert("탈퇴가 완료되었습니다.")
       navigate('/');
     }
@@ -48,11 +61,11 @@ const MypageProfile = () => {
 
         {/* 프로필 사진 및 이름 */}
         <Box className={styles.profileSection}>
-          <Avatar alt={template.memberInfo.profile || 'Profile Picture'}
-            src={template.memberInfo.profile || defaultImage}
+          <Avatar alt={userInfo.profile || 'Profile Picture'}
+            src={userInfo.profile || defaultImage}
             className={styles.avatar} />
           <Typography variant="h5" fontWeight="bold">
-            {template.memberInfo.email || '아이디 없음'}
+            {userInfo.email || '아이디 없음'}
           </Typography>
         </Box>
 
@@ -61,44 +74,44 @@ const MypageProfile = () => {
           <Box className={styles.formGroup}>
             <TextField label="name" variant="filled" fullWidth
               InputProps={{ readOnly: true }}
-              value={template.memberInfo.name || "이름을 기입해주세요."} />
+              value={userInfo.name || "이름을 기입해주세요."} />
             <TextField label="PHONE-NUMBER" variant="filled" fullWidth
               InputProps={{ readOnly: true }}
-              value={template.memberInfo.telNumber || "전화번호를 기입해주세요."} />
+              value={userInfo.telNumber || "전화번호를 기입해주세요."} />
             <Box sx={{ display: 'flex', gap: 2 }}>
               <TextField label="GENDER" variant="filled" fullWidth
                 InputProps={{ readOnly: true }}
-                value={template.memberInfo.gender || "성별를 기입해주세요."} />
+                value={userInfo.gender || "성별를 기입해주세요."} />
               <TextField label="BIRTHDAY" variant="filled" fullWidth
                 InputProps={{ readOnly: true }}
-                value={template.memberInfo.birthday || "생년월일을 기입해주세요."} />
+                value={userInfo.birthday || "생년월일을 기입해주세요."} />
             </Box>
             <TextField label="INTERCITY" variant="filled" fullWidth
               InputProps={{ readOnly: true }}
-              value={regions[template.memberInfo.interCity] || "관심지역을 기입해주세요."} />
+              value={regions[userInfo.interCity] || "관심지역을 기입해주세요."} />
             <Box sx={{ display: 'flex', gap: 2 }}>
               <TextField sx={{ width: 1 }}
                 label="주소" name="address" variant="filled"
                 InputProps={{ readOnly: true }}
-                value={template.memberInfo.address || '주소를 기입주세요.'} />
+                value={userInfo.address || '주소를 기입주세요.'} />
             </Box>
             <Box sx={{ display: 'flex', gap: 2 }}>
               <TextField label="자격증" id="certificate" name="certificate" variant="filled" fullWidth
                 InputProps={{ readOnly: true }}
-                value={template.memberInfo.guidelicense || "자격증 정보가 없습니다."} />
+                value={userInfo.guidelicense || "자격증 정보가 없습니다."} />
             </Box>
             <Grid item xs={12} md={6}>
               <TextField fullWidth label="자기소개" variant="filled" multiline rows={8}
                 InputProps={{ readOnly: true }}
-                value={template.memberInfo.introduce || "자기소개를 기입해주세요."} />
+                value={userInfo.introduce || "자기소개를 기입해주세요."} />
             </Grid>
           </Box>
           <Box sx={{ display: "flex", justifyContent: "space-between", width: '100%' }}>
             <Button onClick={deleteMember} variant="contained"
               sx={{ mt: 2, backgroundColor: '#0066ff', height: '55px', width: '115px', '&:hover': { backgroundColor: '#0056b3' }, }}>
-              삭제 하기
+              탈퇴 하기
             </Button>
-            <Button onClick={() => { navigate(`/mypage/update/${template.memberInfo.id}`) }} variant="contained"
+            <Button onClick={() => { navigate(`/mypage/update/${userInfo.id}`, { state: { userInfo } }) }} variant="contained"
               sx={{ mt: 2, backgroundColor: '#0066ff', height: '55px', width: '115px', '&:hover': { backgroundColor: '#0056b3' }, }}>
               수정 하기
             </Button>
